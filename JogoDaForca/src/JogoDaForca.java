2be1434b88ee1815449aa7974465ae60156853b0
guilherme moraes <guilhermemoraes01.00@gmail.com>
2022-01-20 20:36:58 -0300

    mudanca nos metodos de public para static, retirando OOP adicionado o metodo 'cadastro de tema'

--- JogoDaForca/src/JogoDaForca.java
+++ JogoDaForca/src/JogoDaForca.java
@@ -1,36 +1,38 @@
 import java.util.Scanner;
 public class JogoDaForca {
 	
-	String[][] palavras = new String[51][51];
+	static String[][] palavras = new String[51][51];  
+	
+	// palavras[linhas][0] armazenará os temas cadastradas
+	// palavras[linhas][colunas>0] armazenará as palavras cadastradas
 	
 	public static void main(String[] args) {
 		int option;
-		JogoDaForca game = new JogoDaForca();
 		
 		do {
-		option = game.DisplayMenu();
+		option = DisplayMenu();
 			switch (option){
 			case 1: 
-				game.GerenciarTemas();
+				GerenciarTemas();
 				break;
 			case 2:
-				game.GerenciarPalavras();
+				GerenciarPalavras();
 				break;
 			case 3:
-				game.Jogar();
+				Jogar();
 				break;
 			case 4:
-				game.Sair();
+				Sair();
 				break;
 			default:
 				System.out.print("\nATENÇÃO DIGITE UM NUMERO ENTRE 11 E 4\n");
-				game.DisplayMenu();
+				DisplayMenu();
 				break;
 			}
 		}while(option != 4);
              
 	}
-    public int DisplayMenu() {
+    static int DisplayMenu() {
     	Scanner read = new Scanner(System.in);
     	int option;
     	
@@ -43,18 +45,79 @@ public class JogoDaForca {
     	return option;
         
     }
-    public void GerenciarTemas() {
-    	System.out.println("escolha: gerenciar temas");
-        
-    }
-    public void GerenciarPalavras() {
+    static void GerenciarTemas() {
+    	
+    	/*  O programa deverá ser capaz de armazenar até 50 diferentes temas.
+		(a) Cadastro: o usuário poderá cadastrar temas no programa. Não poderão existir temas com
+			nomes iguais.
+		(b) Exclusão: o usuário poderá excluir temas desde que não existam palavras associadas a ele.
+			Caso não seja possível realizar exclusão, o programa deverá exibir e mensagem “Não foi
+			possível excluir o tema. Verifique se existem palavras cadastradas nesse tema.”
+		(c) Busca: o usuário poderá buscar por temas cadastrados no sistema.
+    	  
+    	 * */
+    	
+    	int option;
+    	int index;
+    	boolean temaRepetido = false;
+    	String temaCadastro;
+    	Scanner read = new Scanner(System.in);
+    	
+    	do {
+    		
+    		System.out.print("\nDigite a opção desejada (1-4)\n");
+    		System.out.print("(1) Cadastrar Tema\n");
+    		System.out.print("(2) Excluir Tema\n");
+    		System.out.print("(3) Buscar por um tema\n");
+    		System.out.print("(4) Voltar ao menu principal\n");
+    		option = read.nextInt();
+    	    
+    		switch (option) {
+    		
+    		case 1:
+    			System.out.print("Digite o tema que deseja cadastrar:\n");
+    			read.nextLine();
+    			temaCadastro = read.nextLine().toString();
+    			temaRepetido = false;
+    			
+    			for( index = 0; index <= 50; index++) {
+    				if(temaCadastro.equals(palavras[index][0]) ){
+    					temaRepetido = true;
+    				    break;
+    				}
+       			}
+    			
+    			if(temaRepetido) {
+    				System.out.print("\ntema já cadastrado por favor digite outro\n");
+    			}else{
+    				for(index = 0; index<50 ;index++) {
+    					if(palavras[index][0] == null){
+    						palavras[index][0] = temaCadastro;
+    	    				System.out.print("\ntema:"+palavras[index][0]+" cadastrado com sucesso\n");
+    	    				break;
+    					}
+    				}	
+    			}
+    			break;
+    		case 2:
+    			
+    		}
+    	
+    	}while(option != 4);        
+    }  
+    
+	private static String String(String teste) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	static void GerenciarPalavras() {
     	System.out.println("escolha: gerenciar palavras");
     	
     }
-    public void Jogar() {
+    static void Jogar() {
     	System.out.println("escolha: jogar");
     }
-    public void Sair() {
+    static void Sair() {
     	System.out.print("\nvoce saiu!\nObrigado por jogar!\n");
     }
     
